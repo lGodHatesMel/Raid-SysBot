4c93656e2bc2422163738d7bd0802e41c6643200
Buggz Evol <56521233+lGodHatesMel@users.noreply.github.com>
2023-05-28 17:50:32 -0400

    setting updates

--- SysBot.Pokemon/SV/BotRotatingRaid/RotatingRaidSettingsSV.cs
+++ SysBot.Pokemon/SV/BotRotatingRaid/RotatingRaidSettingsSV.cs
@@ -15,14 +15,14 @@ namespace SysBot.Pokemon
         private const string FeatureToggle = nameof(FeatureToggle);
         public override string ToString() => "RaidBotSV Settings";
 
-        [Category(FeatureToggle), Description("URL to Pokémon Automation's Tera Ban List json (or one matching the required structure).")]
-        public string BanListURL { get; set; } = "https://raw.githubusercontent.com/PokemonAutomation/ServerConfigs-PA-SHA/main/PokemonScarletViolet/TeraAutoHost-BanList.json";
+        [Category(FeatureToggle), Description("URL link for Tera Ban List json (or one matching the required structure).")]
+        public string BanListURL { get; set; } = "https://raw.githubusercontent.com/lGodHatesMel/SysBot.PokemonScarletViolet/main/Resources/RaidSysBotUserBanlist.JSON";
 
         [Category(Hosting), Description("Amount of raids before updating the ban list. If you want the global ban list off, set this to -1.")]
         public int RaidsBetweenUpdate { get; set; } = 3;
 
         [Category(Hosting), Description("If true, the bot will attempt to auto-generate Raid Parameters from the \"raidsv.txt\" file on botstart.")]
-        public bool GenerateParametersFromFile { get; set; } = true;
+        public bool GenerateParametersFromFile { get; set; } = false;
 
         [Category(Hosting), Description("If true, the bot will attempt to auto-generate Raid Embeds based on the\"preset.txt\" file.")]
         public bool UsePresetFile { get; set; } = true;
@@ -31,7 +31,7 @@ namespace SysBot.Pokemon
         public bool HideRaidCode { get; set; } = true;
 
         [Category(FeatureToggle), Description("If using The `HideRaidCode` Option then add your stream Link URL Here.")]
-        public string RaidStreamLink { get; set; } = "";
+        public string RaidStreamLink { get; set; } = "https://www.twitch.tv/lgodhatesmel";
 
         [Category(Hosting), Description("Raid embed parameters.")]
         public List<RotatingRaidParameters> RaidEmbedParameters { get; set; } = new();
